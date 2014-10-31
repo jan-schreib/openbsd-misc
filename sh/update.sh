1ec85eb9e15b89f2f6ff0d8b309033523caa9d6e
Jan S <jan.schreib@gmail.com>
2014-10-31 15:17:31 +0100

    removed double cd

--- sh/update.sh
+++ sh/update.sh
@@ -4,10 +4,9 @@
 
 export CVSROOT=anoncvs@openbsd.cs.fau.de:/cvs
 
-if [ ! -d /usr/src ]; then
-	cd /usr
+cd /usr
+if [ ! -d src ]; then
 	cvs -qd $CVSROOT get -P src
 else
-	cd /usr
 	cvs -d $CVSROOT up -Pd
 fi
